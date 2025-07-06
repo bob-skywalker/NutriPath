3324e21dd7a3f5b393d0c453e7f4b16fd32ae2c8
Bob <q463250938@gmail.com>
2025-07-06 17:15:05 -0500

    Initial Commit

---
+++ NutriPath/NutriPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriPathApp.swift
+//  NutriPath
+//
+//  Created by Bo Zhong on 7/6/25.
+//
+
+import SwiftUI
+
+@main
+struct NutriPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
